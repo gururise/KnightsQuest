3ab94c4bbf2528724279769bf97c180f7085f741
Gene R <gururise@gmail.com>
2022-12-08 22:27:55 -0800

    Add footstep audio

--- scenes/Character.gd
+++ scenes/Character.gd
@@ -17,24 +17,30 @@ func _physics_process(delta):
 		velocity.x = -WALK_SPEED
 		if is_on_floor():
 			$AnimatedSprite.animation = "walk"
+			if not $FootAudio.playing:
+				$FootAudio.play()
 		$AnimatedSprite.flip_h = true
 	elif Input.is_action_pressed("right") and $AnimatedSprite.animation != "attack":
 		velocity.x = WALK_SPEED
 		if is_on_floor():
 			$AnimatedSprite.animation = "walk"
+			if not $FootAudio.playing:
+				$FootAudio.play()
 		$AnimatedSprite.flip_h = false
 	else:
 		velocity.x = 0
 		if is_on_floor() and $AnimatedSprite.animation == "walk":
 			$AnimatedSprite.animation = "idle"
+			$FootAudio.stop()
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_SPEED
 		$AnimatedSprite.animation = "jump"
+		$FootAudio.stop()
 	
 	if Input.is_action_just_pressed("attack") and is_on_floor():
 		$AnimatedSprite.animation = "attack"
-		#$AudioStreamPlayer.play()
+		$FootAudio.stop()
 		$Timer.start(0.35)
 	
 	move_and_slide(velocity, Vector2.UP)
@@ -43,7 +49,8 @@ func _physics_process(delta):
 func _on_AnimatedSprite_animation_finished():
 	if $AnimatedSprite.animation != "walk":
 		$AnimatedSprite.animation = "idle"
+		$FootAudio.stop()
 
 # Timer Callback for attack audio
 func _on_Timer_timeout():
-	$AudioStreamPlayer.play()
+	$SwordAudio.play()
